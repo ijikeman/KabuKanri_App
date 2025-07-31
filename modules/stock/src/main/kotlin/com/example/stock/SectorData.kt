c6c45af8136ea521bf78985ad5883621d2a504b9
ijikeman <keiji.ue@gmail.com>
2025-07-31 15:38:25 +0000

    add Entities

--- modules/stock/src/main/kotlin/com/example/stock/SectorData.kt
+++ modules/stock/src/main/kotlin/com/example/stock/SectorData.kt
@@ -1,6 +1,11 @@
 package com.example.stock
 
-import jakarta.persistence.*
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+import jakarta.persistence.GenerationType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.Column
 
 @Entity
 @Table(name = "sector_data")
